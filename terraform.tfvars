71bd8d840dbe35b4e5247d1b1490ecb89fe8f0d1
songithub061186 <jersonix@gmai.com>
2024-11-28 05:14:46 +0800

    Not sure Commit

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 vpc_cidr             = "11.0.0.0/16"
 cidr_public_subnet   = ["11.0.1.0/24", "11.0.2.0/24"]
 cidr_private_subnet  = ["11.0.3.0/24", "11.0.4.0/24"]
--- variables.tf
+++ variables.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 variable "vpc_cidr" {
   type        = string
   description = "Public Subnet CIDR values"
@@ -66,72 +65,3 @@ output "dev_proj_1_ec2_instance_public_ip" {
 }
 
 
-=======
-variable "vpc_cidr" {
-  type        = string
-  description = "Public Subnet CIDR values"
-}
-
-variable "cidr_public_subnet" {
-  type        = list(string)
-  description = "Public Subnet CIDR values"
-}
-
-variable "cidr_private_subnet" {
-  type        = list(string)
-  description = "Private Subnet CIDR values"
-}
-
-variable "eu_availability_zone" {
-  type        = list(string)
-  description = "Availability Zones"
-}
-
-variable "vpc_id" {
-  type        = string
-  description = "DevOps Project 1 VPC 1"
-}
-
-
-variable "ec2_sg_name" {
-     type        = string
-     description = "EC2_SG_Name"
-
-}
-
-variable "ec2_jenkins_sg_name" {
-      type        = string
-      description = "Jenkins_SG_Name"
-}
-
-output "sg_ec2_sg_ssh_http_id" {
-  value = aws_security_group.ec2_sg_ssh_http.id
-}
-
-output "sg_ec2_jenkins_port_8080" {
-  value = aws_security_group.ec2_jenkins_port_8080.id
-}
-
-variable "ami_id" {}
-variable "instance_type" {}
-variable "tag_name" {}
-variable "public_key" {}
-variable "subnet_id" {}
-variable "sg_for_jenkins" {}
-variable "enable_public_ip_address" {}
-variable "user_data_install_jenkins" {}
-
-output "ssh_connection_string_for_ec2" {
-  value = format("%s%s", "ssh -i /Users/rahulwagh/.ssh/aws_ec2_terraform ubuntu@", aws_instance.jenkins_ec2_instance_ip.public_ip)
-}
-
-output "jenkins_ec2_instance_ip" {
-  value = aws_instance.jenkins_ec2_instance_ip.id
-}
-
-output "dev_proj_1_ec2_instance_public_ip" {
-  value = aws_instance.jenkins_ec2_instance_ip.public_ip
-}
-
-
->>>>>>> f7aefec7796844488ff78233f49ca266528f4ccb
